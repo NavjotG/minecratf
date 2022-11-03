91459439924e97e1cd7b40e210adf8f4b19cf231
NavjotG <n.ghotra1@share.epsb.ca>
2022-11-03 11:20:33 -0600

    gsrgfs

---
+++ script.js
@@ -0,0 +1,9 @@
+// Variables to Store HTML Elements
+let steveImgE1 = document.getElementById("steve-img");
+let alexImgEl = document.getElementById("alex-img");
+let fish8tnEl = document.getElementById("fish-btn");
+let imgResultEl = document.getElementById("img-result");
+let numCodEl = document.getElementById("num-cod");
+let numSalmonEl = document.getElementById("num-salmon");
+let numTropicalE1 = document.getElementById("num-tropical");
+let numPufferEl = document.getElementById("num-puffer");
