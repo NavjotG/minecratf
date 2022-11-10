0dd6c9371de9a2affdeedb927ca95327e6cadcdd
NavjotG <n.ghotra1@share.epsb.ca>
2022-11-10 11:11:30 -0700

    tewtwtw

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Variables to Store HTML Elements
 let steveImgE1 = document.getElementById("steveP");
 let alexImgEl = document.getElementById("alexP");
-let fish8tnEl = document.getElementById("button-fish");
+let fishbtnEl = document.getElementById("button-fish");
 let imgResultEl = document.getElementById("img-result");
 let numCodEl = document.getElementById("numCod");
 let numSalmonEl = document.getElementById("numSal");
 let numTropicalE1 = document.getElementById("numTro");
 let numPufferEl = document.getElementById("numPuf");
+
+// Event listener
+steveImgE1.addEventListener("click", selectSteve);
+
+function selectSteve() {
+  document.body.style.borderColor = `red`;
+}
