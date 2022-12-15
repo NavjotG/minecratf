b28f360a6ce0bf260be41a10b86a8b04ddc23cd7
NavjotG <n.ghotra1@share.epsb.ca>
2022-12-15 10:32:57 -0700

    gtjtrh

--- script.js
+++ script.js
@@ -8,18 +8,39 @@ let numSalmonEl = document.getElementById("numSal");
 let numTropicalE1 = document.getElementById("numTro");
 let numPufferEl = document.getElementById("numPuf");
 
+// Global Variables
+let character = "Steve";
+
 // Event listener
 steveImgE1.addEventListener("click", selectSteve);
 alexImgEl.addEventListener("click", selectAlex);
+fishbtnEl.addEventListener("click", fishOnce);
 
 function selectSteve() {
+  // Update Active Border
   document.body.style.borderColor = `red`;
   steveImgE1.classList.add("active");
   alexImgEl.classList.remove("active");
+
+  // Update character selection Variable
+  character = "Steve;";
 }
 
 function selectAlex() {
+  // Update Active Border
   document.body.style.borderColor = `red`;
   steveImgE1.classList.remove("active");
   alexImgEl.classList.add("active");
+
+  // Update character selection Variable
+  character = "Alex;";
+}
+
+function fishOnce() {
+  if (character === "Steve") {
+    let randNum = Math.random();
+    console.log(randNum);
+  } else {
+    alert("Fish with Alex");
+  }
 }
